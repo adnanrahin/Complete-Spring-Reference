522c45c4b5be3641e2295c5ca10f0c6a88cabf4d
Adnan Rahin <adnan.rahin13@gmail.com>
2021-06-20 16:00:01 -0400

    Spring Configuration Class Added

--- spring-java-config/src/main/java/com/spring/config/App.java
+++ spring-java-config/src/main/java/com/spring/config/App.java
@@ -1,13 +1,22 @@
 package com.spring.config;
 
+import org.springframework.context.annotation.AnnotationConfigApplicationContext;
+
 /**
  * Hello world!
- *
  */
-public class App 
-{
-    public static void main( String[] args )
-    {
-        System.out.println( "Hello World!" );
+public class App {
+    public static void main(String[] args) {
+
+        AnnotationConfigApplicationContext context = new AnnotationConfigApplicationContext(SportConfig.class);
+
+        Coach baseBallCoach = context.getBean("baseBallCoach", Coach.class);
+
+        System.out.println(baseBallCoach.getDailyWorkout());
+
+        System.out.println(baseBallCoach.getDailyFortune());
+
+        context.close();
+
     }
 }
